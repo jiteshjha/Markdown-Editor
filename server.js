d2e6dde541dece6658eb4bc3fcf0840a27fc8d66
jiteshjha <jiteshjha96@gmail.com>
2015-10-10 18:54:20 +0530

    Added tab spacing

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('pad');
+});
+app.get('/(:id)', function(req, res) {
+  res.render('pad');
+});
+
+var sharejs = require('share');
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+  redisClient = require("redis").createClient();
+}
+
+var options = {
+  db: {type: 'redis'},
+};
+
+sharejs.server.attach(app, options);
+
+var port = process.env.PORT || 8000;
+app.listen(port);
